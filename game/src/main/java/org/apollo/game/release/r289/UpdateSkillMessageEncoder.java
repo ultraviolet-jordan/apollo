0b1f1398fc05a9946f53d69c7f622f0f73e9105b
Jordan <realskillrs@gmail.com>
2020-06-05 07:44:53 -0400

    Remove bad import in update skill packet.

--- game/src/main/java/org/apollo/game/release/r289/UpdateSkillMessageEncoder.java
+++ game/src/main/java/org/apollo/game/release/r289/UpdateSkillMessageEncoder.java
@@ -2,7 +2,6 @@ package org.apollo.game.release.r289;
 
 import org.apollo.game.message.impl.UpdateSkillMessage;
 import org.apollo.game.model.entity.Skill;
-import org.apollo.net.codec.game.DataOrder;
 import org.apollo.net.codec.game.DataType;
 import org.apollo.net.codec.game.GamePacket;
 import org.apollo.net.codec.game.GamePacketBuilder;
