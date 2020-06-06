3101c691fb698f5e9ed9a57d7b3b06d145af0bc7
Jordan <realskillrs@gmail.com>
2020-06-06 19:05:49 -0400

    Remove unused import.

--- game/src/main/java/org/apollo/game/release/r289/FirstItemActionMessageDecoder.java
+++ game/src/main/java/org/apollo/game/release/r289/FirstItemActionMessageDecoder.java
@@ -1,7 +1,6 @@
 package org.apollo.game.release.r289;
 
 import org.apollo.game.message.impl.ItemActionMessage;
-import org.apollo.net.codec.game.DataTransformation;
 import org.apollo.net.codec.game.DataType;
 import org.apollo.net.codec.game.GamePacket;
 import org.apollo.net.codec.game.GamePacketReader;
