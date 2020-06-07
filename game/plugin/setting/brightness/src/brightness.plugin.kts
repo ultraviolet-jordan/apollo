93c5296a8058e722cab88fec78bda9972a3f92f9
Jordan <realskillrs@gmail.com>
2020-06-06 22:56:55 -0400

    Update brightness plugin.

--- game/plugin/setting/brightness/src/brightness.plugin.kts
+++ game/plugin/setting/brightness/src/brightness.plugin.kts
@@ -1,5 +1,6 @@
 import org.apollo.game.message.impl.ButtonMessage
 import org.apollo.game.message.impl.ConfigMessage
+import org.apollo.game.model.entity.Player
 import org.apollo.game.model.entity.setting.ScreenBrightness
 import org.apollo.game.model.event.impl.LoginEvent
 
@@ -12,10 +13,14 @@ on { ButtonMessage::class }
     .where { widgetId == DARK_BUTTON_ID || widgetId == NORMAL_BUTTON_ID || widgetId == BRIGHT_BUTTON_ID || widgetId == VERY_BRIGHT_BUTTON_ID }
     .then { player ->
         player.screenBrightness = ScreenBrightness.valueOf((widgetId - 906) / 2)
-        player.send(ConfigMessage(166, player.screenBrightness.toInteger() + 1))
+        refresh(player)
     }
 
 on_player_event { LoginEvent::class }
     .then { player ->
-        player.send(ConfigMessage(166, player.screenBrightness.toInteger()))
+        refresh(player)
     }
+
+fun refresh(player: Player) {
+    player.send(ConfigMessage(166, player.screenBrightness.toInteger() + 1))
+}
