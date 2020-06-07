3f86e37b861a060c9c969ee8c6a4444a27c45ab2
Jordan <realskillrs@gmail.com>
2020-06-06 20:16:30 -0400

    Update run plugin for updating config during login event.

--- game/plugin/run/src/run.plugin.kts
+++ game/plugin/run/src/run.plugin.kts
@@ -1,4 +1,6 @@
 import org.apollo.game.message.impl.ButtonMessage
+import org.apollo.game.message.impl.ConfigMessage
+import org.apollo.game.model.event.impl.LoginEvent
 
 val WALK_BUTTON_ID = 152
 val RUN_BUTTON_ID = 153
@@ -7,4 +9,9 @@ on { ButtonMessage::class }
     .where { widgetId == WALK_BUTTON_ID || widgetId == RUN_BUTTON_ID }
     .then {
         it.toggleRunning()
+    }
+
+on_player_event { LoginEvent::class }
+    .then { player ->
+        player.send(ConfigMessage(173, if (player.isRunning) 1 else 0))
     }
