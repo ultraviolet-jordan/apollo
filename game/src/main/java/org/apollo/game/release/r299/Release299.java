8a42715701c13c36a88bdad8d0f05d89d2f5699a
Jordan <personal.jordanabraham@gmail.com>
2023-01-07 22:14:34 -0500

    Add NpcSynchronizationMessageEncoder.

--- game/src/main/java/org/apollo/game/release/r299/Release299.java
+++ game/src/main/java/org/apollo/game/release/r299/Release299.java
@@ -2,6 +2,7 @@ package org.apollo.game.release.r299;
 
 import org.apollo.game.message.impl.ConfigMessage;
 import org.apollo.game.message.impl.IdAssignmentMessage;
+import org.apollo.game.message.impl.NpcSynchronizationMessage;
 import org.apollo.game.message.impl.PlayerSynchronizationMessage;
 import org.apollo.game.message.impl.RegionChangeMessage;
 import org.apollo.game.message.impl.ServerChatMessage;
@@ -18,10 +19,10 @@ public class Release299 extends Release {
 	 * The incoming packet lengths array.
 	 */
 	public static final int[] PACKET_LENGTHS = {
-			7, 0, 6, 0, 0, 6, 0, 0, 0, 2, // 0
+			7, 0, 6, 0, 2, 6, 0, 0, 0, 2, // 0
 			0, 0, -1, 8, 0, 0, 0, 0, 0, 0, // 10
-			4, 0, 0, 0, 1, 10, 4, 0, 0, 0, // 20
-			0, 0, 0, 0, 0, 0, 8, -1, 7, 12, // 30
+			4, 0, 2, 0, 1, 10, 4, 0, 0, 0, // 20
+			0, 0, 0, 0, 0, 2, 8, -1, 7, 12, // 30
 			0, 10, 6, 0, 0, 0, 6, -1, 0, 0, // 40
 			0, 0, 1, 4, 0, 0, 0, 0, 4, 0, // 50
 			6, 0, 2, 0, 6, 0, 0, 0, 0, 0, // 60
@@ -36,12 +37,12 @@ public class Release299 extends Release {
 			8, 0, 0, 0, 3, 0, 6, 6, 0, 0, // 150
 			0, 0, 0, 0, 0, 0, 2, 0, 0, 4, // 160
 			0, 0, 0, 0, 2, 0, 0, 0, 0, 6, // 170
-			0, 4, 0, 0, 0, 6, 0, 6, 1, 2, // 180
+			0, 4, 0, 0, 0, 6, 0, 6, 2, 2, // 180
 			0, 0, 0, 0, 0, 8, -1, 0, 0, 0, // 190
 			0, 2, 0, 0, 0, 0, 0, 6, 0, 0, // 200
 			0, 0, 0, 6, 0, 12, 6, 0, 0, 4, // 210
 			0, 0, -1, 2, -1, 0, 0, 0, 0, 0, // 220
-			1, 0, 0, 2, 0, 3, 4, 0, 6, 6, // 230
+			1, 0, 0, 2, 0, 3, 4, 0, 6, 2, // 230
 			4, 0, 3, 0, 0, 0, 0, 1, -1, 0, // 240
 			8, 0, 0, 6, 6, 8 // 250-255
 	};
@@ -73,6 +74,7 @@ public class Release299 extends Release {
 		// encoders
 		register(RegionChangeMessage.class, new RegionChangeMessageEncoder());
 		register(PlayerSynchronizationMessage.class, new PlayerSynchronizationMessageEncoder());
+		register(NpcSynchronizationMessage.class, new NpcSynchronizationMessageEncoder());
 		register(SwitchTabInterfaceMessage.class, new SwitchTabInterfaceMessageEncoder());
 		register(UpdateItemsMessage.class, new UpdateItemsMessageEncoder());
 		register(UpdateSlottedItemsMessage.class, new UpdateSlottedItemsMessageEncoder());
