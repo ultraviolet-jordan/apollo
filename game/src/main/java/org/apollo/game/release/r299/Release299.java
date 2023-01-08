94b75fd4c1263123068d1e139a468046f78e5f13
Jordan <personal.jordanabraham@gmail.com>
2023-01-07 21:58:47 -0500

    Add FirstObjectActionMessageDecoder.

--- game/src/main/java/org/apollo/game/release/r299/Release299.java
+++ game/src/main/java/org/apollo/game/release/r299/Release299.java
@@ -15,7 +15,7 @@ public class Release299 extends Release {
 	 * The incoming packet lengths array.
 	 */
 	public static final int[] PACKET_LENGTHS = {
-			7, 0, 6, 0, 0, 0, 0, 0, 0, 2, // 0
+			7, 0, 6, 0, 0, 6, 0, 0, 0, 2, // 0
 			0, 0, -1, 8, 0, 0, 0, 0, 0, 0, // 10
 			4, 0, 0, 0, 1, 10, 4, 0, 0, 0, // 20
 			0, 0, 0, 0, 0, 0, 8, -1, 7, 12, // 30
@@ -24,10 +24,10 @@ public class Release299 extends Release {
 			6, 0, 2, 0, 6, 0, 0, 0, 0, 0, // 60
 			-1, 0, 2, 1, 6, 0, 0, 0, 0, 0, // 70
 			0, 0, 6, 0, 0, 0, -1, 0, 0, 0, // 80
-			-1, 8, 0, 7, 1, 0, 0, 4, 2, 0, // 90
+			-1, 8, 0, 7, 1, 0, 0, 4, 6, 0, // 90
 			0, 8, 4, 6, 0, 0, 8, 0, 6, 2, // 100
 			0, 0, 0, 0, 0, 0, 0, 6, 0, 0, // 110
-			0, 0, 0, 6, 0, 13, 0, 6, 0, 0, // 120
+			0, 0, 0, 6, 8, 13, 0, 6, 0, 0, // 120
 			0, 0, 0, 0, 0, 8, 12, 10, -1, -1, // 130
 			0, -1, 0, 0, 0, 0, 0, 2, 0, -1, // 140
 			8, 0, 0, 0, 3, 0, 6, 6, 0, 0, // 150
@@ -35,12 +35,12 @@ public class Release299 extends Release {
 			0, 0, 0, 0, 2, 0, 0, 0, 0, 6, // 170
 			0, 4, 0, 0, 0, 6, 0, 6, 1, 2, // 180
 			0, 0, 0, 0, 0, 8, -1, 0, 0, 0, // 190
-			0, 2, 0, 0, 0, 0, 0, -1, 0, 0, // 200
-			0, 0, 0, 6, 0, 6, 6, 0, 0, 4, // 210
+			0, 2, 0, 0, 0, 0, 0, 6, 0, 0, // 200
+			0, 0, 0, 6, 0, 12, 6, 0, 0, 4, // 210
 			0, 0, -1, 2, -1, 0, 0, 0, 0, 0, // 220
-			1, 0, 0, 2, 0, 3, 4, 0, 0, 6, // 230
+			1, 0, 0, 2, 0, 3, 4, 0, 6, 6, // 230
 			4, 0, 3, 0, 0, 0, 0, 1, -1, 0, // 240
-			8, 0, 0, -1, 6, 8 // 250-255
+			8, 0, 0, 6, 6, 8 // 250-255
 	};
 
 	/**
@@ -64,6 +64,7 @@ public class Release299 extends Release {
 		register(37, new PublicChatMessageDecoder());
 		register(62, new ButtonMessageDecoder());
 		register(248, new CommandMessageDecoder());
+		register(238, new FirstObjectActionMessageDecoder());
 
 		// encoders
 		register(RegionChangeMessage.class, new RegionChangeMessageEncoder());
